748a4e5805127fe757c8fd3e7e43546639096354
Eduardo Ferronato <edu.fe.guin@gmail.com>
2022-10-23 12:18:57 -0300

    fix: dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import "./style.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    if (typeof window !== "undefined") localStorage.setItem("chakra-ui-color-mode", "dark")
+  }, [])
+
   return (
     <ChakraProvider theme={extendTheme({config:{useColorMode: "dark"}})}>
       <Component {...pageProps} />
